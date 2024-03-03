01fc5f7ef2b753e7859e56e7e8890db94af44fb9
sf-Abdumalik <abdumaliksafarov03@gmail.com>
2024-03-03 18:35:55 +0500

    Create Project

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.NetworkInformation;
+using System.Collections.Generic;  
+
+using System.Linq;
+
+using System.Text;
+
+using System.Net.WebSockets;
+
+
+namespace Daris_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteString("Assalomu alekum");
+
+            string a = ReturnString("Assalomu", "alekum!");
+            Console.WriteLine(a);
+        }
+
+    }    
+}
